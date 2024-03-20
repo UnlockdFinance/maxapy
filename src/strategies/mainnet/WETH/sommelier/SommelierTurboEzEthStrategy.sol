fb093bc0a48797dd512a623b3c2d1afe47af5deb
Solthodox <xabierblockchain@gmail.com>
2024-03-20 09:38:30 +0100

    sommelier turbo ez-eth

---
+++ src/strategies/mainnet/WETH/sommelier/SommelierTurboEzEthStrategy.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.19;
+import {BaseSommelierStrategy, SafeTransferLib} from "src/strategies/base/BaseSommelierStrategy.sol";
+
+/// @title SommelierTurboEzEthStrategy
+/// @author Adapted from https://github.com/Grandthrax/yearn-steth-acc/blob/master/contracts/strategies.sol
+/// @notice `SommelierTurboEzEthStrategy` supplies an underlying token into a generic Sommelier Vault,
+/// earning the Sommelier Vault's yield
+contract SommelierTurboEzEthStrategy is BaseSommelierStrategy {}
