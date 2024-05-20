099a06c539b6a3406a6141a351d7c0223ac269a4
Solthodox <xabierblockchain@gmail.com>
2024-05-20 12:23:10 +0200

    pessimistic maxWithdraw

--- src/MaxApyVaultV2.sol
+++ src/MaxApyVaultV2.sol
@@ -870,7 +870,7 @@ contract MaxApyVaultV2 is ERC4626, OwnableRoles, ReentrancyGuard {
 
     /// @notice Returns the maximum amount of the underlying asset that can be withdrawn
     /// from the `owner`'s balance in the Vault, via a withdraw call.
-    /*  function maxWithdraw(address owner) public view override returns (uint256 maxAssets) {
+     function maxWithdraw(address owner) public view override returns (uint256 maxAssets) {
         uint256 maxLiquidableAssets = totalIdle;
         for (uint256 i; i < MAXIMUM_STRATEGIES; i++) {
             address strategy = withdrawalQueue[i];
@@ -880,9 +880,11 @@ contract MaxApyVaultV2 is ERC4626, OwnableRoles, ReentrancyGuard {
         uint256 totalSupply = totalSupply();
         // prevent division by zero
         if (totalSupply == 0) return 0;
-        uint256 o = _decimalsOffset();
-        maxAssets = Math.fullMulDiv(maxLiquidableAssets, maxRedeem(owner), totalSupply);
-    } */
+        maxAssets = Math.min(
+            super.maxWithdraw(owner), 
+            Math.fullMulDiv(maxLiquidableAssets, maxRedeem(owner), totalSupply)
+        );
+    }
 
     /// @notice Returns the estimate price of 1 vault share
     function sharePrice() external view returns (uint256) {
--- src/strategies/mainnet/WETH/yearn/YearnAjnaWETHStakingStrategy.sol
+++ src/strategies/mainnet/WETH/yearn/YearnAjnaWETHStakingStrategy.sol
@@ -9,6 +9,7 @@ import {
 } from "src/strategies/base/BaseYearnV3Strategy.sol";
 import { IStakingRewardsMulti } from "src/interfaces/IStakingRewardsMulti.sol";
 import { IUniswapV3Router as IRouter } from "src/interfaces/IUniswap.sol";
+import { FixedPointMathLib as Math } from "solady/utils/FixedPointMathLib.sol";
 
 /// @title YearnAjnaWETHStakingStrategy
 /// @author Adapted from https://github.com/Grandthrax/yearn-steth-acc/blob/master/contracts/strategies.sol
