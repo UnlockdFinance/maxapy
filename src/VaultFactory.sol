cafdde84c6a9bc20e3a25369de0bd4a7def91a45
Solthodox <xabierblockchain@gmail.com>
2024-06-18 14:23:24 +0200

    factory working

--- src/VaultFactory.sol
+++ src/VaultFactory.sol
@@ -3,27 +3,32 @@ pragma solidity ^0.8.19;
 
 import { MaxApyVault } from "./MaxApyVault.sol";
 import { CREATE3 } from "solady/utils/CREATE3.sol";
-import { LibString } from "solady/utils/LibString.sol";
 import { OwnableRoles } from "solady/auth/OwnableRoles.sol";
-import { MatadataReaderLib } from "solady/utils/MetadataReaderLib.sol";
+import { MetadataReaderLib } from "solady/utils/MetadataReaderLib.sol";
 
 contract VaultFactory is OwnableRoles {
-    using LibString for string;
     using MetadataReaderLib for address;
+    ////////////////////////////////////////////////////////////////
+    ///                         EVENTS                           ///
+    ////////////////////////////////////////////////////////////////
+
+    event CreateVault(address indexed asset, address vaultAddress);
+
+    uint256 internal constant _CREATE_VAULT_EVENT_SIGNATURE =
+        0x61d77434230bc4628b3ed22c0c8e26455cdc6f3cafeb2f032ea3b8e375822ab1;
+
     ////////////////////////////////////////////////////////////////
     ///                         CONSTANTS                        ///
     ////////////////////////////////////////////////////////////////
     /// Roles
     uint256 public constant ADMIN_ROLE = _ROLE_0;
     uint256 public constant DEPLOYER_ROLE = _ROLE_1;
-    /// Vault creation code
-    bytes immutable cachedCreationCode;
     /// MaxApy treasury
     address public immutable treasury;
 
     constructor(address _treasury) {
-        /// We cache the vault creation code into storage
-        cachedCreationCode = type(MaxApyVault).creationCode;
+        _initializeOwner(msg.sender);
+        _grantRoles(msg.sender, ADMIN_ROLE);
         treasury = _treasury;
     }
 
@@ -33,16 +38,37 @@ contract VaultFactory is OwnableRoles {
     /// @notice Deploys a vault with a deterministic address
     /// @param underlyingAsset address of the ERC20 deposit token of the vault
     /// @param salt seed hash to compute the new address from
-    function deploy(address underlyingAsset, bytes32 salt) external onlyRoles(DEPLOYER_ROLE) {
+    function deploy(
+        address underlyingAsset,
+        bytes32 salt
+    )
+        external
+        onlyRoles(DEPLOYER_ROLE)
+        returns (address deployed)
+    {
+        // Get asset symbol
         string memory symbol = underlyingAsset.readSymbol();
-        CREATE3.deploy(
+        // Deploy to deterministic address
+        deployed = CREATE3.deploy(
             salt,
             abi.encodePacked(
-                cachedCreationCode,
-                abi.encode(underlyingAsset, "MaxApy-".concat(symbol).concat(" Vault"), "max".concat(symbol), treasury),
-                0
-            )
+                type(MaxApyVault).creationCode, abi.encode(underlyingAsset, parseName(symbol), parseSymbol(symbol), treasury)
+            ),
+            0
         );
+        assembly {
+            // Emit `CreateVault` event
+            mstore(0x00, deployed)
+            log2(0x00, 0x20, _CREATE_VAULT_EVENT_SIGNATURE, underlyingAsset)
+        }
+    }
+
+    function parseName(string memory symbol) private pure returns (string memory) {
+        return string.concat(string.concat("MaxApy-", symbol), " Vault");
+    }
+
+    function parseSymbol(string memory symbol) private pure returns (string memory) {
+        return string.concat("max", symbol);
     }
 
     ////////////////////////////////////////////////////////////////
@@ -51,6 +77,6 @@ contract VaultFactory is OwnableRoles {
     /// @notice Computes the deterministic deployment address of a vault given a salt
     /// @param salt the deployment salt
     function computeAddress(bytes32 salt) external view returns (address) {
-        return getDeployed(salt);
+        return CREATE3.getDeployed(salt);
     }
 }
